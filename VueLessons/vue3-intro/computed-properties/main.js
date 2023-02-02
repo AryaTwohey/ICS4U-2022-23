58276509cef8a1950529f5b32fbf0bc2b5f343fc
Arya Twohey <atwohey@bayviewglen.ca>
2023-02-02 09:14:28 -0500

    Computed Properties

--- VueLessons/vue3-intro/computed-properties/main.js
+++ VueLessons/vue3-intro/computed-properties/main.js
@@ -1,15 +1,14 @@
 const app = Vue.createApp({
    data() {
       return {
+         selectedVariant: 0,
          cart: 0,
          product: 'Socks',
          brand: 'Vue Mastery',
-         image: './assets/images/socks_blue.jpg',
-         inStock: false,
          details: ['50% cotton', '30% wool', '20% polyester'],
          variants: [
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
          ]
       }
    },
@@ -17,8 +16,34 @@ const app = Vue.createApp({
       addToCart() {
          this.cart += 1
       },
-      updateImage(variantImage) {
-         this.image = variantImage
+      updateVariant(index) {
+         this.selectedVariant = index
       }
+   },
+   //these are computed properties
+   //Must use this keyword -> because it refers to 'this instance', as these are reusable componenets
+   //Creates new properties/data that you can use, which are combinations of other properties
+   computed:{
+      title(){
+         return this.brand + ' ' + this.product 
+      },
+      image(){
+         //grabbing the index of each variant and finding the image
+        return this.variants[this.selectedVariant].image
+      },
+      inStock(){ // 0 is considered false along with null, undefined, NaN, " & null"
+         return this.variants[this.selectedVariant].quantity 
+
+      //this returning 0 is saying false
+      },
+      //This computed property, will allow us to use v-show to determine whether or not onsale is true or false
+      onSale(){
+         //if this first condition is true -> do all the other stuff
+         return this.variants[this.selectedVariant].onSale ?  'On Sale' : ''
+      },
+      description(){
+         return this.inStock && this.onSale ? 'In Stock - On Sale' : 'In Stock'
+
+      },
    }
 })
