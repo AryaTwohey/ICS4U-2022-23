f0498d1682f0594b9e6611357369310c66d7b111
Arya Twohey <atwohey@bayviewglen.ca>
2023-02-02 09:40:51 -0500

    Props

--- VueLessons/vue3-intro/components-and-props/main.js
+++ VueLessons/vue3-intro/components-and-props/main.js
@@ -1,15 +1,9 @@
 const app = Vue.createApp({
    data() {
+      //this is like the parent, the product display has everything else
       return {
          cart: 0,
-         product: 'Socks',
-         brand: 'Vue Mastery',
-         selectedVariant: 0,
-         details: ['50% cotton', '30% wool', '20% polyester'],
-         variants: [
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-         ]
+         premium: true,
       }
    },
    methods: {
