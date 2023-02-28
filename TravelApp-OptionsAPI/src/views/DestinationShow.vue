a18c4de6f5112819dbded076a9d4da9fe7df3639
Arya Twohey <atwohey@bayviewglen.ca>
2023-02-28 09:27:31 -0500

    Ajax Request

--- TravelApp-OptionsAPI/src/views/DestinationShow.vue
+++ TravelApp-OptionsAPI/src/views/DestinationShow.vue
@@ -17,14 +17,21 @@
 <script>
 import sourceData from '@/data.json'
 export default {
+    data(){
+        return{
+            destination:null
+        }
+    },
    
     computed:{
         destinationId(){
             return parseInt(this.$route.params.id)
         },
-        destination(){
-            return sourceData.destinations.find(destination => destination.id === this.destinationId)
-        }
+   
+    },
+   async created(){
+        const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}`)
+        this.destination = await response.json()
     }
 }
 </script>
