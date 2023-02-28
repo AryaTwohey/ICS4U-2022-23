8da592a15bdff2b46c941c6fa9102a3daa975571
Arya Twohey <atwohey@bayviewglen.ca>
2023-02-28 08:56:58 -0500

    Fixed TheNavigation Bug

--- TravelApp-OptionsAPI/src/App.vue
+++ TravelApp-OptionsAPI/src/App.vue
@@ -4,21 +4,7 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <div>
-  <div id="nav">
-  <header>
-
-      <br>
-      <router-link to="/">Home </router-link>  
-      <router-link to="/brazil">Brazil </router-link>
-      <router-link to="/hawaii">Hawaii </router-link>
-      <router-link to="/jamacia">Jamacia </router-link>
-      <router-link to="/panama">Panama </router-link>
-
-    
-    
-  
-  </header>
-</div>
+  <TheNavigation/>
 
 <div class="container">
     <router-view></router-view>
