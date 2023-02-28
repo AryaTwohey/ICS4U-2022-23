4696c652d1e9db91486a1731a4a6d45bdef6867d
Arya Twohey <atwohey@bayviewglen.ca>
2023-02-28 09:43:14 -0500

    Fixed Params

--- TravelApp-OptionsAPI/src/views/DestinationShow.vue
+++ TravelApp-OptionsAPI/src/views/DestinationShow.vue
@@ -1,4 +1,32 @@
+<!-- <template>
+    <section class = "destination">
+        <h1>{{ destination.name }}</h1>
+        <div class="destination-details">
+            <img :src="`/images/${destination.image}`" :alt="destination.name">
+            <p>{{ destination.description }}</p>
+        </div>
+    </section>
 
+    <section class="experiences">
+   
+
+</template>
+
+
+<script>
+import sourceData from '@/data.json'
+export default {
+    components:{ExperienceCard},
+    computed:{
+        destinationId(){
+            return parseInt(this.$route.params.id)
+        },
+        destination(){
+            return sourceData.destinations.find(destination => destination.id === this.destinationId)
+        }
+    }
+}
+</script> -->
 <template>
     <section class = "destination">
         <h1>{{ destination.name }}</h1>
@@ -17,21 +45,14 @@
 <script>
 import sourceData from '@/data.json'
 export default {
-    data(){
-        return{
-            destination:null
-        }
-    },
    
     computed:{
         destinationId(){
             return parseInt(this.$route.params.id)
         },
-   
-    },
-   async created(){
-        const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}`)
-        this.destination = await response.json()
+        destination(){
+            return sourceData.destinations.find(destination => destination.id === this.destinationId)
+        }
     }
 }
 </script>
