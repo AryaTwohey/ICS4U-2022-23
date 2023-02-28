82728a4b2b8868d240b7a80942f7632f301e8924
Arya Twohey <atwohey@bayviewglen.ca>
2023-02-28 18:31:18 -0500

    Not Found Page

--- TravelApp-OptionsAPI/src/router/index.js
+++ TravelApp-OptionsAPI/src/router/index.js
@@ -1,32 +1,49 @@
 import { createRouter, createWebHistory } from "vue-router";
-// import HomeView from "../views/HomeView.vue";
-import Home from '@/views/Home.vue'
+
+import Home from "@/views/Home.vue";
+import sourceData from "@/data.json";
 
 const routes = [
-  {path:'/', name:'Home', component:Home},
-  {path:'/:pathMatch(.*)*',name:'NotFound',component:() => import('@/views/NotFound.vue')
-},
+  { path: "/", name: "Home", component: Home },
   {
-    path:'/destination/:id/:slug',
-    name:'destination.show', 
-    component:()=>import('@/views/DestinationShow.vue'),
-    props:route=>({...route.params, id:parseInt(route.params.id)}),
-    children:[
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+  {
+    path: "/destination/:id/:slug",
+    name: "destination.show",
+    component: () => import("@/views/DestinationShow.vue"),
+    props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to, from) {
+      const exists = sourceData.destinations.find(
+        (destination) => destination.id === parseInt(to.params.id)
+      );
+      if (!exists)
+        return {
+          name: "Not Found",
+          params: {
+            pathMatch: to.path.split("/").slice(1),
+            query: to.query,
+            hash: to.hash,
+          },
+        };
+    },
+    children: [
       {
-        path:':experienceSlug',
-        name:'experience.show', 
-        component: ()=>import('@/components/ExperienceShow.vue'),
-        props:route=>({...route.params, id:parseInt(route.params.id)})
-      }
-    ]
-},
-]
+        path: ":experienceSlug",
+        name: "experience.show",
+        component: () => import("@/components/ExperienceShow.vue"),
+        props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes 
+  routes,
   // linkActiveClass:'vue-school-active-link'
-  
 });
 
 export default router;
