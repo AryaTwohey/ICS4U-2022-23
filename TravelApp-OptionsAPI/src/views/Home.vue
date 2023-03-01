4132074f7ff04467f36144a985ad202fc6df96bd
Arya Twohey <atwohey@bayviewglen.ca>
2023-02-28 19:24:28 -0500

    Added Reviews To Home Page

--- TravelApp-OptionsAPI/src/views/Home.vue
+++ TravelApp-OptionsAPI/src/views/Home.vue
@@ -2,6 +2,8 @@
     
     <div class="home">
         <h1>All Destinations</h1>
+
+
         <div class="destinations">
             <router-link v-for="destination in destinations" :key="destination.id" :to="{name:'destination.show', params:{id:destination.id, slug: destination.slug}}">
         
@@ -9,6 +11,16 @@
         <img :src="`/images/${destination.image}`" :alt="destination.name">
         
         </router-link>
+        </div>
+        <br>
+        <center><h2>Reviews</h2></center>
+        <div class="reviews" v-for="review in reviews" :key="review.id">
+            <h3 class="user">{{ review.user }}  posted this</h3>
+            <h2 class="stars">{{ review.stars }}</h2>
+            <div class="user-review">
+            <p class="review">{{ review.review }}</p>
+            </div>
+
         </div>
         
     </div>
@@ -19,12 +31,24 @@
 <script>
 
 import sourceData from '@/data.json'
+import allReviews from '@/reviews.json'
 export default{
 data(){
     return{
-    destinations: sourceData.destinations
+    destinations: sourceData.destinations,
+    reviews: allReviews
 }
 }
 }
 
 </script>
+<style>
+.user-review{
+    border: 1px solid black;
+}
+.review{
+    margin: 10px;
+}
+
+
+</style>
