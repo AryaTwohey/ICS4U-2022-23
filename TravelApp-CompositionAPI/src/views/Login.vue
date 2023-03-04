6e81229c1a619318b9772a0673d44d365a1d7667
Arya Twohey <atwohey@bayviewglen.ca>
2023-03-04 12:47:15 -0500

    Fixed Login Bug

--- TravelApp-CompositionAPI/src/views/Login.vue
+++ TravelApp-CompositionAPI/src/views/Login.vue
@@ -18,13 +18,14 @@ import { useRoute } from 'vue-router';
 <script>
 import {ref} from  'vue'
 import {useRouter, useRoute} from 'vue-router'
-const router = useRouter()
-const route = useRoute()
+
 export default{
 
     setup(){
         const username = ref('')
         const password = ref('')
+        const router = useRouter()
+        const route = useRoute()
         const login = () =>{
             window.user = username.value
             const redirectPath = route.query.redirect || '/protected'
